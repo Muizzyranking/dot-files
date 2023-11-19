4b9e57065245d4deaa807b8afbf9be7dbd6a53ff
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-11-19 06:00:54 -0500

    updated keymapts

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,3 +5,14 @@ local Util = require("lazyvim.util")
 local map = Util.safe_keymap_set
 -- quit
 map("n", "<C-q>", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- floating terminal
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<leader>fT", function()
+  Util.terminal()
+end, { desc = "Terminal (cwd)" })
+map("n", "<c-/>", lazyterm, { desc = "Terminal (cwd)" })
+map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
