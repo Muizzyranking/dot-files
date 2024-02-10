76c664141ced2e261b39a3a77f805c89fa86761b
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-10 01:51:38 +0100

    plugins

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,6 @@ return {
     priority = 1000,
     config = function()
       require("cyberdream").setup({
-        -- Recommended - see "Configuring" below for more config options
         transparent = true,
         italic_comments = true,
         hide_fillchars = true,
@@ -14,12 +13,83 @@ return {
     end,
   },
   {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = true,
+    "folke/tokyonight.nvim",
+    lazy = true,
     opts = {
-      transparent_mode = true,
+      style = "night",
+      transparent = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = { italic = true },
+        variables = { italic = true },
+        sidebar = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      styles = {
+        comments = { "italic" },
+        functions = { "italic" },
+        -- keywords = { "italic" },
+        -- strings = { "italic" },
+        variables = { "italic" },
+      },
+      transparent_background = true,
+      no_italic = false,
+      no_bold = false,
+      integrations = {
+        -- harpoon = true,
+        fidget = true,
+        cmp = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+      -- highlight_overrides = {
+      --   all = function(colors)
+      --     return {
+      --       diagnosticvirtualtexterror = { bg = colors.none },
+      --       diagnosticvirtualtextwarn = { bg = colors.none },
+      --       diagnosticvirtualtexthint = { bg = colors.none },
+      --       diagnosticvirtualtextinfo = { bg = colors.none },
+      --     }
+      --   end,
+      -- },
+      color_overrides = {
+        mocha = {
+          -- i don't think these colours are pastel enough by default!
+          peach = "#fcc6a7",
+          green = "#d2fac5",
+        },
+      },
     },
   },
 }
