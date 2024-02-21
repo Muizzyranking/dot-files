ebc6b8dc6ba3750633548fd1199903ae1f5796df
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-02-21 23:15:09 +0100

    betty style checker

---
+++ nvim/lua/plugins/betty.lua
@@ -0,0 +1,11 @@
+return {
+  "bstevary/betty-in-vim",
+  lazy = true,
+  event = { "BufWritePost *.c" },
+  dependencies = {
+    -- "neovim/nvim-lspconfig",
+    "nvim-lua/plenary.nvim",
+    "dense-analysis/ale",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
