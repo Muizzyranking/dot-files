76615ca3d1f9a5c3b3426c58e7814089cf2afad2
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-03-02 09:13:58 +0100

    plugins

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    cmd = { "ToggleTerm", "TermExec" },
-    opts = {
-      highlights = {
-        Normal = { link = "Normal" },
-        NormalNC = { link = "NormalNC" },
-        NormalFloat = { link = "NormalFloat" },
-        FloatBorder = { link = "FloatBorder" },
-        StatusLine = { link = "StatusLine" },
-        StatusLineNC = { link = "StatusLineNC" },
-        WinBar = { link = "WinBar" },
-        WinBarNC = { link = "WinBarNC" },
-      },
-      size = 10,
-      on_create = function()
-        vim.opt.foldcolumn = "0"
-        vim.opt.signcolumn = "no"
-      end,
-      open_mapping = [[<c-_>]],
-      shading_factor = 2,
-      direction = "float",
-      float_opts = { border = "rounded" },
+    {
+        "akinsho/toggleterm.nvim",
+        cmd = { "ToggleTerm", "TermExec" },
+        opts = {
+            highlights = {
+                Normal = { link = "Normal" },
+                NormalNC = { link = "NormalNC" },
+                NormalFloat = { link = "NormalFloat" },
+                FloatBorder = { link = "FloatBorder" },
+                StatusLine = { link = "StatusLine" },
+                StatusLineNC = { link = "StatusLineNC" },
+                WinBar = { link = "WinBar" },
+                WinBarNC = { link = "WinBarNC" },
+            },
+            size = 10,
+            on_create = function()
+                vim.opt.foldcolumn = "0"
+                vim.opt.signcolumn = "no"
+            end,
+            open_mapping = [[<C-_>]],
+            shading_factor = 2,
+            direction = "float",
+            float_opts = { border = "rounded" },
+        },
     },
-  },
 }
