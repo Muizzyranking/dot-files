e2400160d9d484f7f52323b15cee93084ffa2a72
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-03-02 09:13:48 +0100

    autocommand

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,14 +1,10 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown" },
-  callback = function()
-    vim.wo.conceallevel = 0
-  end,
+    pattern = { "json", "jsonc", "markdown" },
+    callback = function()
+        vim.wo.conceallevel = 0
+    end,
 })
 
 --do not expand tabs in C file
@@ -23,13 +19,13 @@ vim.api.nvim_create_autocmd("FileType", {
 -- )
 
 vim.api.nvim_exec(
-  [[
+    [[
   augroup CFileSettings
     autocmd!
     autocmd BufRead,BufNewFile *.c,*.h setlocal noexpandtab | setlocal tabstop=8 | setlocal shiftwidth=8 | setlocal autoindent | setlocal smartindent
   augroup END
 ]],
-  false
+    false
 )
 
 -- don't auto comment new line
