3c08eea3e4b1f4a687059ddd0581923c095b1a03
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-03 22:50:57 +0100

    mason

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,9 +2,8 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, "emmet-ls")
-      table.insert(opts.ensure_installed, "html-lsp")
-      table.insert(opts.ensure_installed, "css-lsp")
+      table.insert(opts.ensure_installed, "prettierd")
+      table.insert(opts.ensure_installed, "prettier")
       -- table.insert(opts.ensure_installed, "python-lsp-server")
     end,
   },
