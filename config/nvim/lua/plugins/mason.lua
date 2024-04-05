6565a93ecaf47129eb73a568d627465a74deb52b
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-05 22:37:29 +0100

    ensure sql formatter is installed

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -4,6 +4,8 @@ return {
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "prettierd")
       table.insert(opts.ensure_installed, "prettier")
+      table.insert(opts.ensure_installed, "sql-formatter")
+
       -- table.insert(opts.ensure_installed, "python-lsp-server")
     end,
   },
