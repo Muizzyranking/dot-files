d0913a1274818e801c339f404c0801f109eb5d8b
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-04-11 12:49:20 +0100

    custom comment

--- config/nvim/lua/plugins/mini-comment.lua
+++ config/nvim/lua/plugins/mini-comment.lua
@@ -9,23 +9,22 @@ return {
         --use custom comment for c files
         if filetype == "c" then
           return "/*%s*/" -- Return desired comment style for C
-        else
-          return nil
+        end
+
+        -- for some reason sql comment doesnt work
+        -- after setting custom comment for c
+        if filetype == "sql" then
+          return "--%s" --
         end
       end,
     },
     mappings = {
-      comment = comment,
 
       -- Toggle comment on current line
       comment_line = comment,
 
       -- Toggle comment on visual selection
       comment_visual = comment,
-
-      -- Define 'comment' textobject (like `dgc` - delete whole comment block)
-      -- Works also in Visual mode if mapping differs from `comment_visual`
-      textobject = comment,
     },
   },
 }
