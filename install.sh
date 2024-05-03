3f31a10516c2d9bfecb08726dd71cfb1d90f9276
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-03 10:47:08 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
 # Update and upgrade the package manager
-sudo dnf update -y
-sudo dnf upgrade -y
+sudo dnf update -y -qq
+sudo dnf upgrade -y -qq
+
+# File containing the list of packages
+package_list_file="packages"
+
+# Loop through each package in the file
+while read -r package; do
+    # Check if the package is already installed
+    if dnf list installed "$package" &> /dev/null; then
+        echo "Package $package is already installed"
+    else
+        # Check if the package is available in the repositories
+        if dnf info "$package" &> /dev/null; then
+            echo -e "Installing package $package\n"
+            sudo dnf install -y -q "$package"
+            echo -e "\nInstalled package $package"
+        else
+            echo "Package $package not found, skipping..."
+        fi
+    fi
+done < "$package_list_file"
+
 
 # Install packages from list
-curl https://raw.githubusercontent.com/Muizzyranking/dot-files/master/packages | xargs sudo dnf install -y
-sudo dnf copr enable atim/lazygit -y
-sudo dnf install lazygit -y
+sudo dnf copr enable atim/lazygit -y -q
+sudo dnf install lazygit -y -q
 
 echo "Installation completed."
 
@@ -27,4 +47,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "Installing Tmux Plugin Manager (TPM)..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-source ~/.zshrc
+source "$HOME"/.zshrc
