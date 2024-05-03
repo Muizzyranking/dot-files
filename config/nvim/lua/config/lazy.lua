affdcb36f6734c9f858d2f2adace4e0eecfd1e13
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-03 13:38:58 +0100

    imported mini animate

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -39,7 +39,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.dap.nlua" },
 
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.editor.leap" },
 
     -- { import = "lazyvim.plugins.extras.lang.go" },
