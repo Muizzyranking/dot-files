85c0058a371dccc6f07500fa406849ebb423218d
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-03 13:38:44 +0100

    keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,8 @@
+local create_newfile = require("config.custom-func").new_file
 -- local del = vim.keymap.del
 local set = vim.keymap.set
+
+set("n", "<leader>fn", create_newfile, { desc = "New file", noremap = true, silent = true })
 
 set("n", "x", '"_x')
 
@@ -26,20 +29,22 @@ set("i", "<C-b>", "<esc>I", { noremap = true, silent = true })
 --go to next line in insert mode
 set("i", "<C-o>", "<esc>o")
 
---dont copy when pasting over text
-set("v", "p", '"_dP', { remap = true, silent = true })
+-- dont copy when pasting over text
+-- not working for some reasons, use P instead
+-- TODO: check why this is not working
+set("v", "p", '"_dP')
+set("x", "p", '"_dP')
 
---dont copy when changing text
-
---run betty on the current file
--- set("n", "<leader>rb", "<cmd>!betty %<cr>", { silent = true, desc = "Run betty on current file" })
-
---run pycodestyle on the current file
--- set("n", "<leader>rp", "<cmd>!pycodestyle %<cr>", { desc = "Run pycodestyle on current file" })
-
+-- better indenting
 --using <tab> and <s-tab> for indenting and dedenting
-set("v", "<tab>", ">", { desc = "indent", remap = true, silent = true })
-set("v", "<s-tab>", "<", { desc = "dedent", remap = true, silent = true })
+set("v", "<S-Tab>", "<gv", { noremap = false, silent = true })
+set("v", "<Tab>", ">gv", { noremap = false, silent = true })
 
 -- make file executable
-set("n", "<leader>cx", "<cmd>!chmod +x %<cr>", { desc = "Make file executable", silent = true })
+set("n", "<leader>cx", "<cmd>!chmod +x % >/dev/null 2>&1<cr>", { desc = "Make file executable", silent = true })
+
+-- Move to window using the <ctrl> hjkl keys in terminal mode
+set("t", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+set("t", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+set("t", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+set("t", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
