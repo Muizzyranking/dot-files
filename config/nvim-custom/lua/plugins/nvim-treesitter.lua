a1bbce79fad1483cad01ff2372f7e2acc496dcc7
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-08 23:19:15 +0100

    treesitter

--- config/nvim-custom/lua/plugins/nvim-treesitter.lua
+++ config/nvim-custom/lua/plugins/nvim-treesitter.lua
@@ -1,54 +1,141 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	version = false,
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"luadoc",
-				"python",
-				"toml",
-				"ninja",
-				"rst",
-				"html",
-				"css",
-				"javascript",
-				"sql",
-			},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
+    -- init = function(plugin)
+    --   -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    --   -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    --   -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    --   -- Luckily, the only things that those plugins need are the custom queries, which we make available
+    --   -- during startup.
+    --   require("lazy.core.loader").add_to_rtp(plugin)
+    --   require("nvim-treesitter.query_predicates")
+    -- end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+    opts = {
+      textobjects = {
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = { query = "@class.outer", desc = "Next class start" },
+            --
+            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+            ["]o"] = "@loop.*",
+            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+            --
+            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+          -- Below will go to either the start or the end, whichever is closer.
+          -- Use if you want more granular movements
+          -- Make it even more gradual by adding multiple queries and regex.
+          goto_next = {
+            ["]d"] = "@conditional.outer",
+          },
+          goto_previous = {
+            ["[d"] = "@conditional.outer",
+          },
+        },
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-\\>",
+          node_incremental = "<C-\\>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      ensure_installed = {
+        "c",
+        "cpp",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "luadoc",
+        "python",
+        "toml",
+        "ninja",
+        "rst",
+        "html",
+        "css",
+        "javascript",
+        "sql",
+        "rst",
+        "toml",
+      },
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
 
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = false,
 
-			indent = { true },
-			auto_tag = { true },
+      indent = { true },
+      auto_tag = { true },
 
-			highlight = {
-				enable = true,
+      highlight = {
+        enable = true,
 
-				-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
 
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  -- treesitter context
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
+    enabled = true,
+    opts = { mode = "cursor", max_lines = 3 },
+    keys = {
+      {
+        "<leader>ut",
+        function()
+          local tsc = require("treesitter-context")
+          local util = require("config.util")
+          tsc.toggle()
+          if util.get_upvalue(tsc.toggle, "enabled") then
+            util.info("Enabled Treesitter Context", { title = "Option" })
+          else
+            util.warn("Disabled Treesitter Context", { title = "Option" })
+          end
+        end,
+        desc = "Toggle Treesitter Context",
+      },
+    },
+  },
 }
