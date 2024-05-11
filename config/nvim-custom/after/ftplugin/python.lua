5fcbf803f12fd3939e0ec7f91730733a77f096b2
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-11 15:07:10 +0100

    removed

--- config/nvim-custom/after/ftplugin/python.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "<leader>co", function()
-  vim.lsp.buf.code_action({
-    apply = true,
-    context = {
-      only = { "source.organizeImports" },
-      diagnostics = {},
-    },
-  })
-end, { noremap = true, silent = true, desc = "Organize Imports" })
