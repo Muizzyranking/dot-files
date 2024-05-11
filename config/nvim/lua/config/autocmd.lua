a27461be237774376b342fffde79cf2acd192e31
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-11 14:13:32 +0100

    autocommands

---
+++ config/nvim/lua/config/autocmd.lua
@@ -0,0 +1,100 @@
+-- Create an augroup with the given name and clear it
+local function augroup(name)
+  return vim.api.nvim_create_augroup("Muizzyvim_" .. name, { clear = true })
+end
+
+-----------------------------------------------------------
+-- Go to the last cursor position when opening a buffer
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-----------------------------------------------------------
+-- Enable spell checking for certain file types
+-- TODO: make this options in ftplugins
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("spell_check"),
+  pattern = { "*.txt", "*.md", "*.tex" },
+  callback = function()
+    vim.opt.spell = true
+    vim.opt.spelllang = "en"
+  end,
+})
+
+-----------------------------------------------------------
+-- Highlight yanked (copied) text
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-----------------------------------------------------------
+-- Close certain file types with q
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+    "Telescope",
+    "telescope",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-----------------------------------------------------------
+-- Auto create directory when saving a file
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-----------------------------------------------------------
+-- Don't auto comment new line
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-----------------------------------------------------------
+-- Make it easier to close man-files when opened inline
+-----------------------------------------------------------
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
