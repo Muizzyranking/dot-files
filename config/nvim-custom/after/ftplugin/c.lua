cd851909c47c320531b7988418e35b62d3db78cc
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-11 15:07:10 +0100

    removed

--- config/nvim-custom/after/ftplugin/c.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8
-vim.opt.softtabstop = 8
-vim.opt.expandtab = false
-
-vim.keymap.set("n", "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header (C/C++)" })
