826b1c96bc76c68128e8dd69576d228551be1cd3
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-11 15:05:13 +0100

    servers

---
+++ config/nvim/lua/config/servers.lua
@@ -0,0 +1,143 @@
+local M = {}
+
+M.lsp = {
+  -- lsp config here to mak things less cluttered
+  clangd = {
+    root_dir = function(fname)
+      return require("lspconfig.util").root_pattern(
+        "Makefile",
+        "configure.ac",
+        "configure.in",
+        "config.h.in",
+        "meson.build",
+        "meson_options.txt",
+        "build.ninja"
+      )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require(
+        "lspconfig.util"
+      ).find_git_ancestor(fname)
+    end,
+    capabilities = {
+      offsetEncoding = { "utf-16" },
+    },
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--header-insertion=iwyu",
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "--fallback-style=llvm",
+      "--offset-encoding=utf-16",
+    },
+    init_options = {
+      usePlaceholders = true,
+      completeUnimported = true,
+      clangdFileStatus = true,
+    },
+    setup = {
+      clangd = function(_, opts)
+        local util = require("lazy.util")
+        local clangd_ext_opts = util.opts("clangd_extensions.nvim")
+        require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+        return false
+      end,
+    },
+  },
+  pyright = {
+    settings = {
+      pyright = {
+        -- Using Ruff's import organizer
+        disableOrganizeImports = true,
+      },
+      python = {
+        analysis = {
+          -- Ignore all files for analysis to exclusively use Ruff for linting
+          ignore = { "*" },
+        },
+      },
+    },
+  },
+  ruff_lsp = {
+    on_attach = function(client, bufnr)
+      if client.name == "ruff_lsp" then
+        client.server_capabilities.hoverProvider = false
+      end
+    end,
+  },
+
+  marksman = {},
+
+  emmet_ls = {},
+  cssls = {},
+  html = {},
+  sqlls = {},
+  bashls = {
+    filetypes = { "sh", "zsh", "bash" },
+  },
+  eslint = {
+    settings = {
+      -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+      workingDirectories = { mode = "auto" },
+    },
+  },
+
+  tsserver = {
+    ---@diagnostic disable-next-line: missing-fields
+    settings = {
+      completions = {
+        completeFunctionCalls = true,
+      },
+    },
+  },
+
+  jsonls = {
+    -- lazy-load schemastore when needed
+    on_new_config = function(new_config)
+      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+      vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    end,
+    settings = {
+      json = {
+        format = {
+          enable = true,
+        },
+        validate = { enable = true },
+      },
+    },
+  },
+
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = { version = "LuaJIT" },
+        workspace = {
+          checkThirdParty = false,
+          -- Tells lua_ls where to find all the Lua files that you have loaded
+          -- for your neovim configuration.
+          library = {
+            "${3rd}/luv/library",
+            unpack(vim.api.nvim_get_runtime_file("", true)),
+          },
+          -- If lua_ls is really slow on your computer, you can try this instead:
+          -- library = { vim.env.VIMRUNTIME },
+        },
+        completion = {
+          callSnippet = "Replace",
+        },
+      },
+    },
+  },
+}
+
+M.fts_n_linters = {
+  "stylua", -- Used to format lua code
+  "autopep8",
+  "prettier",
+  "prettierd",
+  "sql-formatter",
+  "flake8",
+  "shellcheck",
+  "shfmt",
+}
+
+return M
