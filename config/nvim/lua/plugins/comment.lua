fbf63ace53336e652acd7816a292c92014c402c1
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-12 12:58:19 +0100

    comments

---
+++ config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,65 @@
+local comment = "<leader>/"
+return {
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    config = true,
+    keys = {
+      {
+        "]t",
+        function()
+          require("todo-comments").jump_next()
+        end,
+        desc = "Next Todo Comment",
+      },
+      {
+        "[t",
+        function()
+          require("todo-comments").jump_prev()
+        end,
+        desc = "Previous Todo Comment",
+      },
+      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
+  -- mini comment (toggle comments)
+  -- TODO:  Change to numToStr/comment.nvim
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      lazy = true,
+      opts = {
+        enable_autocmd = false,
+      },
+    },
+    opts = {
+      options = {
+        custom_commentstring = function()
+          local filetype = vim.bo.filetype -- Get the current filetype
+          --use custom comment for c files
+          if filetype == "c" then
+            return "/*%s*/"
+          end
+          -- for some reason sql comment doesnt work
+          -- after setting custom comment for c
+          if filetype == "sql" then
+            return "--%s"
+          end
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+      mappings = {
+        -- Toggle comment on current line
+        comment_line = comment,
+        -- Toggle comment on visual selection
+        comment_visual = comment,
+      },
+    },
+  },
+}
