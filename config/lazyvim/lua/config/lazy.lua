bc198b60eaa5cf666231d42cf079b374580fb820
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-06-12 23:40:12 +0100

    lazyvim

--- config/lazyvim/lua/config/lazy.lua
+++ config/lazyvim/lua/config/lazy.lua
@@ -49,6 +49,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.vscode" },
     { import = "plugins" },
   },
   defaults = {
