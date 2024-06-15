b6e51c72a2e37f77847a13c83471067aea8cdc0d
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-06-15 10:44:11 +0100

    ft plugin

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<leader>cv", "<cmd>VenvSelect<cr>", { desc = "Select VirtualEnv" })
+-- vim.keymap.set("n", "<leader>cv", "<cmd>VenvSelect<cr>", { desc = "Select VirtualEnv" })
 
 vim.g.disable_autoformat = false -- enable autoformat on save in python
