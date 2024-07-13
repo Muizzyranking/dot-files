871577c5f059108f95c3137b431d6a5e3eb536e8
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-07-13 15:52:43 +0100

    ftplugin

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -4,8 +4,6 @@ local utils = require("utils")
 -- vim.bo.tabstop = 4
 vim.b.disable_autoformat = false
 vim.g.disable_autoformat = false
-
-vim.g.disable_autoformat = false -- enable autoformat on save in python
 if utils.has("venv-selector.nvim") then
   vim.api.nvim_buf_set_keymap(0, "n", "<leader>cv", "<cmd>VenvSelect<cr>", { desc = "Select VirtualEnv" })
 end
