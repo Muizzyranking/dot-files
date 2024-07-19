8f835356473a845eb88680fbadfff290f62345e5
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-07-19 16:06:05 +0100

    nvim

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -1,9 +1,2 @@
-local utils = require("utils")
-
--- vim.bo.shiftwidth = 4
--- vim.bo.tabstop = 4
 vim.b.disable_autoformat = false
 vim.g.disable_autoformat = false
-if utils.has("venv-selector.nvim") then
-  vim.api.nvim_buf_set_keymap(0, "n", "<leader>cv", "<cmd>VenvSelect<cr>", { desc = "Select VirtualEnv" })
-end
--- config/nvim/lua/plugins/lang/python.lua
+++ config/nvim/lua/plugins/lang/python.lua
@@ -2,7 +2,7 @@ return {
   {
     "linux-cultist/venv-selector.nvim",
     cmd = "VenvSelect",
-    -- event = "VeryLazy",
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
     opts = {
       dap_enabled = false,
       name = {
