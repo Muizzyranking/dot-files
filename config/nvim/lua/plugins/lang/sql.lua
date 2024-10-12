cf15ded43304052082e5e572e5b4ff880268aa67
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-10-12 18:06:06 +0100

    sql

---
+++ config/nvim/lua/plugins/lang/sql.lua
@@ -0,0 +1,79 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    cmd = "DB",
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    dependencies = "vim-dadbod",
+    ft = { "sql", "mysql", "plsql" },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function()
+          local cmp = require("cmp")
+
+          -- global sources
+          ---@param source cmp.SourceConfig
+          local sources = vim.tbl_map(function(source)
+            return { name = source.name }
+          end, cmp.get_config().sources)
+
+          -- add vim-dadbod-completion source
+          table.insert(sources, { name = "vim-dadbod-completion" })
+
+          -- update sources for the current buffer
+          cmp.setup.buffer({ sources = sources })
+        end,
+      })
+    end,
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    dependencies = "vim-dadbod",
+    keys = {
+      { "<leader>z", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+    init = function()
+      local data_path = vim.fn.stdpath("data")
+
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+
+      -- NOTE: The default behavior of auto-execution of queries on save is disabled
+      -- this is useful when you have a big query that you don't want to run every time
+      -- you save the file running those queries can crash neovim to run use the
+      -- default keymap: <leader>S
+      vim.g.db_ui_execute_on_save = false
+    end,
+  },
+  -- Edgy integration
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = "Database",
+        ft = "dbui",
+        pinned = true,
+        width = 0.3,
+        open = function()
+          vim.cmd("DBUI")
+        end,
+      })
+
+      opts.bottom = opts.bottom or {}
+      table.insert(opts.bottom, {
+        title = "DB Query Result",
+        ft = "dbout",
+      })
+    end,
+  },
+}
