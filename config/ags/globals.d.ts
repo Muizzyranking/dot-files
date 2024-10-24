1150fed81b5448ee65d7941826f5f7e95c795c9c
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-10-24 19:08:53 +0100

    ags

--- config/ags/globals.d.ts
+++ config/ags/globals.d.ts
@@ -1,13 +1,14 @@
 // globals.d.ts
 /* eslint-disable no-var */
 
-import { Options, Variable as VariableType } from "types/variable";
+import { Options, Variable as VariableType } from 'types/variable';
 
 declare global {
-  var globalMousePos: VariableType<number[]>;
-  var useTheme: (filePath: string) => void;
-  var options: Options;
-  var removingNotifications: VariableType<boolean>;
+    var useTheme: (filePath: string) => void;
+    var isWindowVisible: (windowName: string) => boolean;
+    var globalWeatherVar: VariableType<Weather>;
+    var options: Options;
+    var removingNotifications: VariableType<boolean>;
 }
 
 export {};
