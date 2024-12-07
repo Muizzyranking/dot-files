f276dea428e9323806ec2e1fa8509e738abfad85
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-07 17:10:56 +0100

    switch to snacks dashboard
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- config/nvim/lua/plugins/ui/dashboard.lua
+++ config/nvim/lua/plugins/ui/dashboard.lua
@@ -1,40 +1,47 @@
-local dashboard = {
-  "nvimdev/dashboard-nvim",
-  event = "VimEnter",
-  opts = function()
-    local opts = {
-      theme = "doom",
-      hide = {
-        -- this is taken care of by lualine
-        -- enabling this messes up the actual laststatus setting after loading a file
-        statusline = false,
-      },
-      config = {
-        -- header = vim.split(logo, "\n"),
-        center = {
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = Utils.ui.logo.my,
+        keys = {
           {
+            icon = " ",
+            key = "n",
+            desc = "New File",
             action = function()
               Utils.keys.new_file()
             end,
-            desc = " New file",
-            icon = " ",
-            key = "n",
           },
           {
-            -- action = "Telescope oldfiles",
+            icon = " ",
+            key = "f",
+            desc = "Find File",
             action = function()
-              require("telescope.builtin").oldfiles({
-                prompt_title = "Recent Files",
+              require("telescope.builtin").find_files({
+                cwd = Utils.find_root_directory(0, { ".git", "lua" }),
                 layout_config = {
                   preview_width = 0.6,
                 },
               })
             end,
-            desc = " Recent files",
-            icon = " ",
-            key = "R",
           },
           {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = function()
+              require("telescope.builtin").live_grep({
+                layout_config = {
+                  preview_width = 0.6,
+                },
+              })
+            end,
+          },
+          {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files(cwd)",
             action = function()
               require("telescope.builtin").oldfiles({
                 prompt_title = "Recent Files in current working directory",
@@ -44,30 +51,24 @@ local dashboard = {
                 },
               })
             end,
-            desc = " Recent files (cwd)",
-            icon = " ",
-            key = "r",
           },
           {
+            icon = " ",
+            key = "R",
+            desc = "Recent Files",
             action = function()
-              require("telescope.builtin").find_files({
-                cwd = Utils.find_root_directory(0, { ".git", "lua" }),
+              require("telescope.builtin").oldfiles({
+                prompt_title = "Recent Files",
                 layout_config = {
                   preview_width = 0.6,
                 },
               })
             end,
-            desc = " Find files",
-            icon = " ",
-            key = "f",
           },
           {
-            action = 'lua require("persistence").load()',
-            desc = " Restore Session",
-            icon = " ",
-            key = "s",
-          },
-          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
             action = function()
               require("telescope.builtin").find_files({
                 cwd = vim.fn.stdpath("config"),
@@ -77,48 +78,22 @@ local dashboard = {
                 },
               })
             end,
-            desc = " Config",
-            icon = " ",
-            key = "c",
           },
           {
-            action = "Lazy",
-            desc = " Lazy",
-            icon = "󰒲 ",
-            key = "l",
+            icon = " ",
+            key = "s",
+            desc = "Restore Session",
+            section = "session",
           },
-          {
-            action = "qa",
-            desc = " Quit",
-            icon = " ",
-            key = "q",
-          },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
-        footer = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-        end,
       },
-    }
-
-    for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-      button.key_format = "  %s"
-    end
-
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == "lazy" then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "DashboardLoaded",
-        callback = function()
-          require("lazy").show()
-        end,
-      })
-    end
-
-    return opts
-  end,
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
 }
-return {}
