ad6c1f1e9fa495a5d99ca9ef905184eba84f8a89
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-13 19:15:43 +0100

    replace notify

---
+++ config/nvim/lua/plugins/ui/notifier.lua
@@ -0,0 +1,49 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>n",
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = "Notification History",
+    },
+    {
+      "<leader>un",
+      function()
+        Snacks.notifier.hide()
+      end,
+      desc = "Dismiss All Notifications",
+    },
+  },
+  opts = {
+    notifier = {
+      timeout = 3000,
+      width = { min = 40, max = 0.4 },
+      height = { min = 1, max = 0.6 },
+      margin = { top = 0, right = 1, bottom = 0 },
+      padding = true,
+      sort = { "level", "added" },
+      level = vim.log.levels.TRACE,
+      icons = {
+        error = " ",
+        warn = " ",
+        info = " ",
+        debug = " ",
+        trace = " ",
+      },
+      keep = function(notif)
+        return vim.fn.getcmdpos() > 0
+      end,
+      style = "compact",
+      top_down = false, -- place notifications from top to bottom
+      date_format = "%R", -- time format for notifications
+      -- format for footer when more lines are available
+      -- `%d` is replaced with the number of lines.
+      -- only works for styles with a border
+      ---@type string|boolean
+      more_format = " ↓ %d lines ",
+      refresh = 50,
+    },
+  },
+}
