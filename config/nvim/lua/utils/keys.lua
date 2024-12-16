f88b1a80eed9abb50f2c769451a855730b678473
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-16 02:44:49 +0100

    nvim cleanup

--- config/nvim/lua/utils/keys.lua
+++ config/nvim/lua/utils/keys.lua
@@ -127,10 +127,8 @@ local function split_into_words(str)
     end
     return words
   end
-
   local words = {}
   local current_word = ""
-
   for i = 1, #str do
     local char = str:sub(i, i)
     if char:match("[A-Z]") and current_word ~= "" then
@@ -140,11 +138,9 @@ local function split_into_words(str)
       current_word = current_word .. char
     end
   end
-
   if current_word ~= "" then
     table.insert(words, current_word:lower())
   end
-
   return words
 end
 
@@ -162,73 +158,137 @@ local function to_camel_case(words)
 end
 
 ----------------------------------------------------
+--- Convert array of words to PascalCase
+---@param words string[] Array of words to convert
+---@return string The PascalCase string
+----------------------------------------------------
+local function to_pascal_case(words)
+  local result = ""
+  for _, word in ipairs(words) do
+    result = result .. word:sub(1, 1):upper() .. word:sub(2)
+  end
+  return result
+end
+
+----------------------------------------------------
 --- Convert array of words to snake_case
---- @param words string[] Array of words to convert
---- @return string The snake_case string
+---@param words string[] Array of words to convert
+---@return string The snake_case string
 ----------------------------------------------------
 local function to_snake_case(words)
   return table.concat(words, "_")
 end
 
 ----------------------------------------------------
---- Convert a word between camelCase and snake_case
---- @param word string The word to convert
---- @return string The converted word
+--- Detect the current case style of a word
+---@param word string The word to analyze
+---@return string The detected case style
 ----------------------------------------------------
-local function convert_case(word)
-  local words = split_into_words(word)
+local function detect_case_style(word)
   if word:find("_") then
-    return to_camel_case(words)
+    return "snake_case"
+  elseif word:sub(1, 1):match("[A-Z]") then
+    return "PascalCase"
   else
+    return "camelCase"
+  end
+end
+
+----------------------------------------------------
+--- Convert a word to the specified case style
+---@param word string The word to convert
+---@param target_case string The target case style
+---@return string The converted word
+----------------------------------------------------
+local function convert_to_case(word, target_case)
+  local words = split_into_words(word)
+  if target_case == "camelCase" then
+    return to_camel_case(words)
+  elseif target_case == "PascalCase" then
+    return to_pascal_case(words)
+  else -- snake_case
     return to_snake_case(words)
   end
 end
 
 ----------------------------------------------------
 --- Check if LSP is available and supports rename for current buffer
---- @return boolean Whether LSP with rename support is available
+---@return boolean Whether LSP with rename support is available
 ----------------------------------------------------
 local function has_rename_support()
   local bufnr = vim.api.nvim_get_current_buf()
   if not vim.api.nvim_buf_is_valid(bufnr) or not vim.bo[bufnr].buflisted then
     return false
   end
-
   return Utils.lsp.has(bufnr, "rename")
+end
+
+----------------------------------------------------
+--- Show case style selection menu and apply conversion
+---@param current_word string The word to convert
+---@param current_case string The current case style
+----------------------------------------------------
+local function show_case_selection(current_word, current_case)
+  local case_styles = {
+    { text = "camelCase", value = "camelCase" },
+    { text = "PascalCase", value = "PascalCase" },
+    { text = "snake_case", value = "snake_case" },
+  }
+
+  -- Remove current case style from options
+  for i, style in ipairs(case_styles) do
+    if style.value == current_case then
+      table.remove(case_styles, i)
+      break
+    end
+  end
+
+  vim.ui.select(case_styles, {
+    prompt = "Select target case style:",
+    format_item = function(item)
+      return item.text
+    end,
+  }, function(choice)
+    if choice then
+      local new_word = convert_to_case(current_word, choice.value)
+
+      if has_rename_support() then
+        local bufnr = vim.api.nvim_get_current_buf()
+        local clients = Utils.lsp.get_clients({
+          bufnr = bufnr,
+          method = "textDocument/rename",
+        })
+        if #clients > 0 then
+          if Utils.has("inc-rename.nvim") then
+            vim.cmd("IncRename " .. new_word)
+          else
+            vim.lsp.buf.rename(new_word)
+          end
+        else
+          local cmd = string.format("normal! ciw%s", new_word)
+          vim.cmd(cmd)
+          Utils.notify.warn("LSP detached, performed local rename only", { title = "Case Toggle" })
+        end
+      else
+        local cmd = string.format("normal! ciw%s", new_word)
+        vim.cmd(cmd)
+      end
+    end
+  end)
 end
 
 ----------------------------------------------------
 --- Toggle case of word under cursor with LSP support
 --- Uses LSP rename when available, falls back to local change
 ----------------------------------------------------
-function M.toggle_case()
+function M.change_var_case()
   local current_word = vim.fn.expand("<cword>")
   if current_word == "" then
     return
   end
 
-  local new_word = convert_case(current_word)
-  if has_rename_support() then
-    local bufnr = vim.api.nvim_get_current_buf()
-    local clients = Utils.lsp.get_clients({
-      bufnr = bufnr,
-      method = "textDocument/rename",
-    })
-    if #clients > 0 then
-      if Utils.has("inc-rename.nvim") then
-        vim.cmd("IncRename " .. new_word)
-      else
-        vim.lsp.buf.rename(new_word)
-      end
-    else
-      local cmd = string.format("normal! ciw%s", new_word)
-      vim.cmd(cmd)
-      Utils.notify.warn("LSP detached, performed local rename only", { title = "Case Toggle" })
-    end
-  else
-    local cmd = string.format("normal! ciw%s", new_word)
-    vim.cmd(cmd)
-  end
+  local current_case = detect_case_style(current_word)
+  show_case_selection(current_word, current_case)
 end
 
 return M
