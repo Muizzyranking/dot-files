017f022b445eec3f20170c4bd22224cbb8261a34
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-19 18:28:04 +0100

    replaced colorify with colorizer

---
+++ config/nvim/lua/plugins/ui/colorizer.lua
@@ -0,0 +1,41 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = function()
+    local colorizer = require("colorizer")
+    local bufnr = vim.api.nvim_get_current_buf()
+    Utils.toggle_map({
+      "<leader>uh",
+      get_state = function()
+        return colorizer.is_buffer_attached(bufnr) > 1
+      end,
+      change_state = function(state)
+        colorizer[state and "detach_from_buffer" or "attach_to_buffer"](bufnr)
+      end,
+      name = "Color highlight",
+    })
+    return {
+      filetypes = { "*" },
+      user_default_options = {
+        names = true,
+        RGB = true,
+        RRGGBB = true,
+        RRGGBBAA = false,
+        AARRGGBB = false,
+        rgb_fn = false,
+        hsl_fn = false,
+        css = false,
+        css_fn = false,
+        mode = "virtualtext",
+        tailwind = "both",
+        sass = { enable = false, parsers = { "css" } },
+        virtualtext = "",
+        virtualtext_inline = true,
+        virtualtext_mode = "foreground",
+        always_update = false,
+      },
+      buftypes = {},
+      user_commands = true,
+    }
+  end,
+}
