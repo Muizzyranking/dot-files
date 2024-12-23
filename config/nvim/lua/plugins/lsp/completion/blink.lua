fc6dcdace3525165ac31d493e4235e66d6317030
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-23 10:00:38 +0100

    changes to completion engine

--- config/nvim/lua/plugins/lsp/completion/blink.lua
+++ config/nvim/lua/plugins/lsp/completion/blink.lua
@@ -10,7 +10,6 @@ return {
       "rafamadriz/friendly-snippets",
       {
         "saghen/blink.compat",
-        optional = true,
         opts = {},
         version = "*",
       },
@@ -22,45 +21,49 @@ return {
       "sources.default",
       "sources.compat",
       "sources.completion.enabled_providers",
+      "disable_ft",
     },
     opts = {
+      -- list of filetypes to be disabled
+      disable_ft = {},
+      snippets = {
+        expand = function(snippet, _)
+          return Utils.cmp.expand(snippet)
+        end,
+      },
       keymap = {
         preset = "enter",
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
         -- I use <c-e> to go to the end of the line in insert mode
         ["<C-e>"] = {},
         -- a as in abort makes sense to me
         ["<C-a>"] = { "hide", "fallback" },
         ["<CR>"] = { "accept", "fallback" },
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback" },
-        ["<C-n>"] = { "select_next", "fallback" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-y>"] = { "select_and_accept" },
       },
-
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
         kind_icons = Utils.icons.kinds,
       },
-
+      signature = { enabled = true },
       sources = {
         default = { "lsp", "snippets", "buffer", "path" },
         compat = {},
-        -- optionally disable cmdline completions
-        -- cmdline = {},
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          -- Search forward and backward
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          -- Commands
+          if type == ":" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
       },
       completion = {
-        trigger = {
-          show_on_insert_on_trigger_charater = true,
-        },
         accept = {
-          -- experimental auto-brackets support
           auto_brackets = {
             enabled = true,
           },
@@ -69,33 +72,29 @@ return {
           border = "rounded",
           winblend = 0,
           draw = {
-            treesitter = true,
-            columns = { { "kind_icon" }, { "label", "label_description" }, { "source_name", gap = 1 } },
+            treesitter = { "lsp" },
+            columns = { { "kind_icon" }, { "label", "label_description" }, { "kind", gap = 1 } },
             components = {
-              label_description = {
-                width = { max = 30 },
+              kind = {
+                ellipsis = false,
+                width = { fill = true },
                 text = function(ctx)
-                  return ctx.label_description
+                  return ("(%s)"):format(ctx.kind)
                 end,
-                highlight = "BlinkCmpLabelDescription",
-              },
-              source_name = {
-                width = { max = 30 },
-                text = function(ctx)
-                  -- return ctx.source_name
-                  return ("(%s)"):format(ctx.source_name)
+                highlight = function(ctx)
+                  return (require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx) or "BlinkCmpKind")
+                    .. ctx.kind
                 end,
-                highlight = "Comment",
               },
             },
           },
         },
-
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 200,
           window = {
             border = "rounded",
+            winblend = 0,
           },
         },
         ghost_text = {
@@ -115,8 +114,15 @@ return {
           table.insert(enabled, source)
         end
       end
-      opts.sources.completion = opts.sources.completion or {}
-      opts.sources.completion.enabled_providers = enabled
+      local disabled_filetypes = opts.disable_ft or {}
+      opts.enabled = function()
+        -- will use to disable completions on certain filetypes
+        return not vim.tbl_contains(disabled_filetypes, vim.bo.filetype)
+          and vim.bo.buftype ~= "prompt"
+          and vim.b.completion ~= false
+      end
+      opts.sources.compat = nil
+      opts.disable_ft = nil
 
       for _, provider in pairs(opts.sources.providers or {}) do
         if provider.kind then
@@ -134,16 +140,10 @@ return {
             end
             return items
           end
+          provider.kind = nil
         end
       end
       require("blink.cmp").setup(opts)
     end,
-  },
-  {
-    "catppuccin",
-    optional = true,
-    opts = {
-      integrations = { blink_cmp = true },
-    },
   },
 }
