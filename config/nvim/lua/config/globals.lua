7a5d87e2503151006f5cc254d89068ad420686cf
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-24 09:57:51 +0100

    configs

--- config/nvim/lua/config/globals.lua
+++ config/nvim/lua/config/globals.lua
@@ -1,3 +1,15 @@
+-- global variables
+_G.Utils = require("utils")
+function _G.should_use_blink()
+  if vim.g.use_cmp then
+    return false
+  end
+  if (vim.g.use_cmp == nil or vim.g.use_cmp ~= false) and vim.g.use_blink then
+    return true
+  end
+  return false
+end
+
 -- stylua: ignore start
 vim.g.mapleader                          = " "
 vim.g.maplocalleader                     = "\\"
