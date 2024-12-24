93485d81859cd5a1ace540bb2fafba2e41027ca8
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-12-24 10:02:22 +0100

    Added eslint

---
+++ config/nvim/lua/plugins/lang/eslint.lua
@@ -0,0 +1,43 @@
+return Utils.setup_lang({
+  name = "eslint",
+  ft = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "json",
+    "vue",
+    "svelte",
+    "markdown",
+    "html",
+    "mjs",
+    "cjs",
+  },
+  lsp = {
+    servers = {
+      eslint = {
+        settings = {
+          workingDirectories = { mode = "auto" },
+        },
+      },
+    },
+    setup = {
+      eslint = function()
+        Utils.lsp.on_attach(function(_, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            buffer = bufnr,
+            callback = function()
+              local cl = Utils.lsp.get_clients({ name = "eslint", bufnr = bufnr })[1]
+              if cl then
+                local diag = vim.diagnostic.get(bufnr, { namespace = vim.lsp.diagnostic.get_namespace(cl.id) })
+                if #diag > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end,
+          })
+        end, "eslint")
+      end,
+    },
+  },
+})
