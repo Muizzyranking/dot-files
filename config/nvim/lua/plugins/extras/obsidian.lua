387885b17142d3dc270e4d6d31bee924ad4840b3
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-01-05 14:03:35 +0100

    extras

--- config/nvim/lua/plugins/extras/obsidian.lua
+++ config/nvim/lua/plugins/extras/obsidian.lua
@@ -1,41 +1,3 @@
-vim.api.nvim_create_augroup("ObsidianKeymaps", { clear = true })
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("ObsidianKeymaps", { clear = true }),
-  pattern = "/home/muizzyranking/Documents/Notes/*",
-  callback = function(event)
-    local maps = {
-      {
-        "<leader>on",
-        function()
-          vim.cmd("ObsidianNew")
-        end,
-        desc = "Create a new note in zettelkasten",
-        icon = { icon = "󰈔 " },
-      },
-      {
-        "<leader>ot",
-        function()
-          vim.cmd("ObsidianTemplate")
-        end,
-        desc = "Insert a template",
-        icon = { icon = " ", color = "yellow" },
-      },
-      {
-        "<leader>oc",
-        function()
-          vim.cmd("ObsidianNewFromTemplate")
-        end,
-        desc = "Create a new note from a template",
-        icon = { icon = "󰎞 ", color = "yellow" },
-      },
-    }
-    for _, map in ipairs(maps) do
-      map.buffer = event.buf
-    end
-    Utils.map(maps)
-  end,
-})
-
 local plugins = {}
 table.insert(plugins, {
   "epwalsh/obsidian.nvim",
@@ -50,112 +12,149 @@ table.insert(plugins, {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/Documents/Notes/",
-      },
-    },
-    notes_subdir = "Zettelkasten",
-    templates = {
-      folder = "Templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M",
-      substitutions = {},
-    },
-
-    log_level = vim.log.levels.INFO,
-    daily_notes = {
-      folder = "notes/daily",
-      date_format = "%Y-%m-%d",
-      -- Optional, if you want to change the date format of the default alias of daily notes.
-      alias_format = "%B %-d, %Y",
-      default_tags = { "daily-notes" },
-      template = nil,
-    },
-    completion = {
-      nvim_cmp = not should_use_blink(),
-      min_chars = 2,
-    },
-    mappings = {
-      ["gf"] = {
-        action = function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
-      },
-      ["<leader>ch"] = {
-        action = function()
-          return require("obsidian").util.toggle_checkbox()
-        end,
-        opts = { buffer = true },
-      },
-    },
-    new_notes_location = "notes_subdir",
-    preferred_link_style = "wiki",
-    disable_frontmatter = true,
-    note_id_func = function(title)
-      local suffix = ""
-      if title ~= nil then
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
+  opts = function()
+    vim.api.nvim_create_augroup("ObsidianKeymaps", { clear = true })
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("ObsidianKeymaps", { clear = true }),
+      pattern = "/home/muizzyranking/Documents/Notes/*",
+      callback = function(event)
+        local maps = {
+          {
+            "<leader>on",
+            function()
+              vim.cmd("ObsidianNew")
+            end,
+            desc = "Create a new note in zettelkasten",
+            icon = { icon = "󰈔 " },
+          },
+          {
+            "<leader>ot",
+            function()
+              vim.cmd("ObsidianTemplate")
+            end,
+            desc = "Insert a template",
+            icon = { icon = " ", color = "yellow" },
+          },
+          {
+            "<leader>oc",
+            function()
+              vim.cmd("ObsidianNewFromTemplate")
+            end,
+            desc = "Create a new note from a template",
+            icon = { icon = "󰎞 ", color = "yellow" },
+          },
+        }
+        for _, map in ipairs(maps) do
+          map.buffer = event.buf
         end
-      end
-      -- return tostring(os.time()) .. "-" .. suffix
-      return suffix
-    end,
+        Utils.map(maps)
+      end,
+    })
+    return {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/Notes/",
+        },
+      },
+      notes_subdir = "Zettelkasten",
+      templates = {
+        folder = "Templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+        substitutions = {},
+      },
 
-    sort_by = "modified",
-    sort_reversed = true,
-    search_max_lines = 1000,
-    open_notes_in = "current",
-    ui = {
-      enable = false,
-    },
-    -- Specify how to handle attachments.
-    attachments = {
-      img_folder = "assets/imgs",
-      img_name_func = function()
-        return string.format("%s-", os.time())
+      log_level = vim.log.levels.INFO,
+      daily_notes = {
+        folder = "notes/daily",
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+        default_tags = { "daily-notes" },
+        template = nil,
+      },
+      completion = {
+        nvim_cmp = false,
+        min_chars = 2,
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+      new_notes_location = "notes_subdir",
+      preferred_link_style = "wiki",
+      disable_frontmatter = true,
+      note_id_func = function(title)
+        local suffix = ""
+        if title ~= nil then
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        -- return tostring(os.time()) .. "-" .. suffix
+        return suffix
       end,
-      img_text_func = function(client, path)
-        path = client:vault_relative_path(path) or path
-        return string.format("![%s](%s)", path.name, path)
-      end,
-    },
-  },
+
+      sort_by = "modified",
+      sort_reversed = true,
+      search_max_lines = 1000,
+      open_notes_in = "current",
+      ui = {
+        enable = false,
+      },
+      -- Specify how to handle attachments.
+      attachments = {
+        img_folder = "assets/imgs",
+        img_name_func = function()
+          return string.format("%s-", os.time())
+        end,
+        img_text_func = function(client, path)
+          path = client:vault_relative_path(path) or path
+          return string.format("![%s](%s)", path.name, path)
+        end,
+      },
+    }
+  end,
 })
 
-if should_use_blink() and Utils.is_in_notes_dir() then
-  table.insert(plugins, {
-    "saghen/blink.cmp",
-    dependencies = {
-      { "epwalsh/obsidian.nvim", "saghen/blink.compat" },
-    },
-    opts = {
-      sources = {
-        compat = { "obsidian", "obsidian_new", "obsidian_tags" },
-        providers = {
-          obsidian = {
-            kind = "Obsidian",
-            async = true,
-          },
-          obsidian_new = {
-            kind = "Obsidian",
-            async = true,
-          },
-          obsidian_tags = {
-            kind = "Obsidian",
-            async = true,
-          },
+table.insert(plugins, {
+  "saghen/blink.cmp",
+  dependencies = {
+    { "epwalsh/obsidian.nvim", "saghen/blink.compat" },
+  },
+  opts = {
+    sources = {
+      compat = { "obsidian", "obsidian_new", "obsidian_tags" },
+      providers = {
+        obsidian = {
+          kind = "Obsidian",
+          async = true,
+        },
+        obsidian_new = {
+          kind = "Obsidian",
+          async = true,
+        },
+        obsidian_tags = {
+          kind = "Obsidian",
+          async = true,
         },
       },
     },
-  })
-end
+  },
+})
 
 if Utils.is_in_notes_dir() then
   table.insert(plugins, {
