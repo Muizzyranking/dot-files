9df467122a7a71c4c18545dcdf9ca758b3797ed5
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-01-05 13:59:07 +0100

    removed nvim cmp from copilot

--- config/nvim/lua/plugins/ai/copilot.lua
+++ config/nvim/lua/plugins/ai/copilot.lua
@@ -14,33 +14,6 @@ return {
     },
   },
   {
-    "nvim-cmp",
-    optional = true,
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          -- attach cmp source whenever copilot attaches
-          Utils.lsp.on_attach(function()
-            -- fixes lazy-loading issues with the copilot cmp source
-            copilot_cmp._on_insert_enter({})
-          end, "copilot")
-        end,
-      },
-    },
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-  {
     "saghen/blink.cmp",
     optional = true,
     dependencies = { "giuxtaposition/blink-cmp-copilot" },
