6918f6c4346b6dd7248b082263672e112dc7b145
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-02-09 18:27:46 +0100

    remove compat and enable cmdline completion

--- config/nvim/lua/plugins/lsp/blink.lua
+++ config/nvim/lua/plugins/lsp/blink.lua
@@ -3,18 +3,12 @@ return {
     "saghen/blink.cmp",
     dependencies = {
       "rafamadriz/friendly-snippets",
-      {
-        "saghen/blink.compat",
-        opts = {},
-        version = "*",
-      },
     },
     version = "v0.*",
     event = "InsertEnter",
     -- build = 'cargo build --release',
     opts_extend = {
       "sources.default",
-      "sources.compat",
       "sources.completion.enabled_providers",
       "disable_ft",
     },
@@ -33,23 +27,24 @@ return {
         -- a as in abort makes sense to me
         ["<C-a>"] = { "hide", "fallback" },
         ["<C-y>"] = { "select_and_accept" },
-        -- cmdline = {
-        --   preset = "enter",
-        --   ["<CR>"] = { "accept", "fallback" },
-        --   ["<C-y>"] = { "select_and_accept" },
-        --   ["<esc>"] = {
-        --     "hide",
-        --     "cancel",
-        --     -- HACK:stop esc from exeuting commands in insert mode
-        --     -- from https://github.com/Saghen/blink.cmp/issues/547
-        --     function()
-        --       if vim.fn.getcmdtype() ~= "" then
-        --         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", true)
-        --         return
-        --       end
-        --     end,
-        --   },
-        -- },
+        cmdline = {
+          preset = "enter",
+          ["<tab>"] = {},
+          ["<CR>"] = { "accept", "fallback" },
+          ["<C-y>"] = { "select_and_accept" },
+          ["<esc>"] = {
+            "hide",
+            "cancel",
+            -- HACK:stop esc from exeuting commands in insert mode
+            -- from https://github.com/Saghen/blink.cmp/issues/547
+            function()
+              if vim.fn.getcmdtype() ~= "" then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", true)
+                return
+              end
+            end,
+          },
+        },
       },
       appearance = {
         use_nvim_cmp_as_default = true,
@@ -59,12 +54,11 @@ return {
       signature = { enabled = false },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
-        compat = {},
-        -- disable for now, it is messing with tab completion
         -- TODO: try later
-        cmdline = {},
+        -- cmdline = {},
       },
       completion = {
+        list = { selection = { preselect = true, auto_insert = false } },
         accept = {
           auto_brackets = {
             enabled = true,
@@ -72,6 +66,9 @@ return {
         },
         menu = {
           border = "rounded",
+          auto_show = function(ctx)
+            return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
+          end,
           winblend = 0,
           draw = {
             treesitter = { "lsp" },
@@ -105,16 +102,6 @@ return {
       },
     },
     config = function(_, opts)
-      for _, source in ipairs(opts.sources.compat or {}) do
-        opts.sources.providers[source] = vim.tbl_deep_extend(
-          "force",
-          { name = source, module = "blink.compat.source" },
-          opts.sources.providers[source] or {}
-        )
-        if type(opts.sources.default) == "table" and not vim.tbl_contains(opts.sources.default, source) then
-          table.insert(opts.sources.default, source)
-        end
-      end
       local disabled_filetypes = opts.disable_ft or {}
       opts.enabled = function()
         -- will use to disable completions on certain filetypes
