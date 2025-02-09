7b777f1442ae9b1d70b4de3588ca81370bf981a6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-02-09 22:28:13 +0100

    nvim

--- config/nvim/lua/plugins/lsp/trouble.lua
+++ config/nvim/lua/plugins/lsp/trouble.lua
@@ -16,14 +16,8 @@ return {
     { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
     { "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
     { "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", desc = "Diagnostics (Trouble)" },
-    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
-    {
-      "<leader>cS",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP references/definitions/... (Trouble)",
-    },
-    -- { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
-    -- { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+   { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
     {
       "[q",
       function()
