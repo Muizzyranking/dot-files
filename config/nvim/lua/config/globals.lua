94dc71712aebf2d5f3bde6a67f9f2e4f3ec06f43
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-03-03 23:17:55 +0100

    nvim config

--- config/nvim/lua/config/globals.lua
+++ config/nvim/lua/config/globals.lua
@@ -1,4 +1,5 @@
 -- stylua: ignore start
+vim.hl = vim.highlight or vim.hl
 vim.g.mapleader                          = " "
 vim.g.maplocalleader                     = "\\"
 vim.g.bigfile                            = 1.5 * 1024 * 1024 -- 1.5MB
@@ -6,3 +7,15 @@ vim.g.bigfile_max_lines                  = 32768
 vim.g.netrw_browsex_viewer               = os.getenv("BROWSER")
 vim.hl.priorities.semantic_tokens = 95
 vim.g.autoformat                         = false
+vim.g.clipboard = {
+  name = 'WL-Clipboard',
+  copy = {
+    ['+'] = 'wl-copy --type text/plain',
+    ['*'] = 'wl-copy --primary --type text/plain',
+  },
+  paste = {
+    ['+'] = 'wl-paste --no-newline',
+    ['*'] = 'wl-paste --primary --no-newline',
+  },
+  cache_enabled = 1,
+}
