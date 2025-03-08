13f8c7cc8cfcc3e0418f4d053844148ff7a49799
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-03-08 22:18:34 +0100

    big file treesitter

--- config/nvim/lua/plugins/lsp/nvim-treesitter.lua
+++ config/nvim/lua/plugins/lsp/nvim-treesitter.lua
@@ -80,16 +80,9 @@ return {
 
       highlight = {
         enable = true,
-
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
+        disable = function(_, buf)
+          return vim.b[buf].bigfile or vim.fn.win_gettype() == "command"
         end,
-
         additional_vim_regex_highlighting = false,
       },
     },
