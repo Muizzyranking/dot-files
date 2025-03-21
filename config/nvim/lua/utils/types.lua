e64f8fe1f4ab7379bef8b532e9ad0590b508026c
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-03-21 23:58:28 +0100

    types

--- config/nvim/lua/utils/types.lua
+++ config/nvim/lua/utils/types.lua
@@ -16,14 +16,14 @@
 ---@field custom_servers? table # Additional LSP options
 
 ---@class setup_lang.formatting
----@field formatters? table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride> Custom formatters and overrides for built-in formatters.
----@field formatters_by_ft? table<string, conform.FiletypeFormatter> Map of filetype to formatters
+---@field formatters? table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride> # Custom formatters and overrides for built-in formatters.
+---@field formatters_by_ft? table<string, conform.FiletypeFormatter> # Map of filetype to formatters
 ---@field format_on_save? boolean # Whether to format on save
 ---@field use_prettier_biome? boolean|string[] # Whether to use prettier for this filetypes or use prettier for specific filetypes
 
 ---@class setup_lang.linting
 ---@field linters_by_ft? table<string, string[]> # Linters by filetype
----@field linters? table<string, lint.Linter|fun():lint.Linter># linters
+---@field linters? table<string, lint.Linter|fun():lint.Linter> # linters
 
 ---@class setup_lang.treesitter
 ---@field parsers? string[] # Parser names to install
@@ -51,22 +51,22 @@
 ---@field commentstring? string|table<string, any> # Comment string configuration
 
 ---@class color_converter.color_pattern
----@field pattern string The strict validation pattern
----@field regex string The buffer matching regex
+---@field pattern string # The strict validation pattern
+---@field regex string # The buffer matching regex
 
 ---@class color_converter.parsers
----@field hex function Hex color parser
----@field rgb function RGB color parser
----@field rgba function RGBA color parser
----@field hsl function HSL color parser
----@field hsla function HSLA color parser
+---@field hex function # Hex color parser
+---@field rgb function # RGB color parser
+---@field rgba function # RGBA color parser
+---@field hsl function # HSL color parser
+---@field hsla function # HSLA color parser
 
 ---@class color_converter.formatters
----@field hex function Hex formatter
----@field rgb function RGB formatter
----@field rgba function RGBA formatter
----@field hsl function HSL formatter
----@field hsla function HSLA formatter
+---@field hex function # Hex formatter
+---@field rgb function # RGB formatter
+---@field rgba function # RGBA formatter
+---@field hsl function # HSL formatter
+---@field hsla function # HSLA formatter
 
 ---@alias KeymapMode
 ---| '"n"' # Normal mode
@@ -79,26 +79,26 @@
 ---| '"c"' # Command mode
 
 ---@class map.KeymapOpts
----@field [1] string The left-hand side of the mapping
----@field [2] string|function The right-hand side of the mapping
----@field mode? KeymapMode|KeymapMode[] Vim mode(s) for the mapping
----@field desc? string|function Description of the mapping
----@field buffer? number Buffer number for buffer-local mapping
----@field silent? boolean Whether the mapping should be silent
----@field remap? boolean Whether the mapping should be remappable
----@field expr? boolean Whether the mapping is an expression
----@field unique? boolean Whether to error on duplicate mappings
----@field icon? string|table Icon for which-key integration
+---@field [1] string # The left-hand side of the mapping
+---@field [2] string|function # The right-hand side of the mapping
+---@field mode? KeymapMode|KeymapMode[] # Vim mode(s) for the mapping
+---@field desc? string|function # Description of the mapping
+---@field buffer? number # Buffer number for buffer-local mapping
+---@field silent? boolean # Whether the mapping should be silent
+---@field remap? boolean # Whether the mapping should be remappable
+---@field expr? boolean # Whether the mapping is an expression
+---@field unique? boolean # Whether to error on duplicate mappings
+---@field icon? string|table # Icon for which-key integration
 
 ---@class map.ToggleOpts
----@field [1] string The left-hand side of the mapping
----@field name string Name of the toggle
----@field get_state function Function that returns the current state
----@field change_state? function Function to change the state
----@field toggle_fn? function Custom toggle function
----@field mode? KeymapMode|KeymapMode[] Vim mode(s) for the mapping
----@field desc? string|function Description of the mapping
----@field icon? table Icon configuration
----@field color? table Color configuration
----@field notify? boolean Whether to show notifications
----@field set_key? boolean Whether to set the keymap immediately
+---@field [1] string # The left-hand side of the mapping
+---@field name string # Name of the toggle
+---@field get_state function # Function that returns the current state
+---@field change_state? function # Function to change the state
+---@field toggle_fn? function # Custom toggle function
+---@field mode? KeymapMode|KeymapMode[] # Vim mode(s) for the mapping
+---@field desc? string|function # Description of the mapping
+---@field icon? table # Icon configuration
+---@field color? table # Color configuration
+---@field notify? boolean # Whether to show notifications
+---@field set_key? boolean # Whether to set the keymap immediately
