d0ffc84c0c3546ad87106cf339bf85bf72aa34b6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-03-30 07:50:40 +0100

    change event

--- config/nvim/lua/plugins/ai/copilot.lua
+++ config/nvim/lua/plugins/ai/copilot.lua
@@ -3,7 +3,7 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     build = ":Copilot auth",
-    event = "InsertEnter",
+    event = "BufReadPost",
     opts = {
       suggestion = { enabled = false },
       panel = { enabled = false },
