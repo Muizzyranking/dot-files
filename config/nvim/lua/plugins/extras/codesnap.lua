3d8bb50095610ede2eb1343f47ef503e6a07ffa6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-04-01 22:18:03 +0100

    plugin

--- config/nvim/lua/plugins/extras/codesnap.lua
+++ config/nvim/lua/plugins/extras/codesnap.lua
@@ -2,6 +2,11 @@ return {
   "mistricky/codesnap.nvim",
   build = "make",
   cmd = { "CodeSnap" },
+  init = function()
+    if vim.fn.readable("~/Pictures/nvim") == 0 then
+      vim.fn.mkdir("~/Pictures/nvim")
+    end
+  end,
   keys = {
     -- { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
     { "<leader>cp", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
