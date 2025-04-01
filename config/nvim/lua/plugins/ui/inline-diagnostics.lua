2af812b6cd1fa5c94b428c3b283519eb1024bd36
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-04-01 22:17:24 +0100

    hl

--- config/nvim/lua/plugins/ui/inline-diagnostics.lua
+++ config/nvim/lua/plugins/ui/inline-diagnostics.lua
@@ -3,6 +3,27 @@ return {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "LazyFile",
     priority = 1000,
-    opts = {},
+    opts = {
+      preset = "modern",
+      transparent_bg = false,
+      hi = {
+        error = "InlineDiagnosticError",
+        warn = "InlineDiagnosticWarn",
+        info = "InlineDiagnosticInfo",
+        hint = "InlineDiagnosticHint",
+      },
+
+      options = {
+        use_icons_from_diagnostic = true,
+        show_all_diags_on_cursorline = false,
+        -- Custom format function for diagnostic messages
+        -- Example:
+        -- format = function(diagnostic)
+        --     return diagnostic.message .. " [" .. diagnostic.source .. "]"
+        -- end
+        format = nil,
+      },
+      disabled_ft = {},
+    },
   },
 }
