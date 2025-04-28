0a9dcacdd840f88a1c66250d905b8f268d076c5e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-04-28 03:44:32 +0100

    grapple

--- config/nvim/lua/plugins/editor/grapple/init.lua
+++ config/nvim/lua/plugins/editor/grapple/init.lua
@@ -1,25 +1,74 @@
 return {
   "cbochs/grapple.nvim",
-  dependencies = {},
-  keys = function()
-    local keys = {
+  event = "VeryLazy",
+  opts = {
+    scope = "lsp",
+  },
+  config = function(_, opts)
+    require("grapple").setup(opts)
+
+    local function set_index_keys()
+      for i = 1, 5 do
+        pcall(function()
+          vim.keymap.del("n", "<leader>m" .. i)
+          Utils.map.add_to_wk({
+            {
+              "<leader>m" .. i,
+              desc = "removed",
+            },
+          })
+        end)
+      end
+      local numeric_keys = {}
+      for i = 1, 5 do
+        if require("grapple").exists({ index = i }) then
+          table.insert(numeric_keys, {
+            "<leader>m" .. i,
+            function()
+              require("grapple").select({ index = i })
+            end,
+            desc = "Goto bookmark " .. i,
+          })
+        end
+      end
+
+      if #numeric_keys > 0 then
+        Utils.map.set_keymaps(numeric_keys, { icon = "󰓹 ", color = "green" })
+      end
+    end
+    local function grapple_update_maps(method, options)
+      options = options or {}
+      method = method or "tag"
+      local ok, result = pcall(function()
+        require("grapple")[method](options)
+        set_index_keys()
+      end)
+      if ok then
+        return result
+      end
+    end
+    Utils.map.add_to_wk({
       {
         "<leader>m",
-        desc = "Grapple: Toggle bookmark",
+        desc = "Bookmarks",
+        icon = { icon = "󰓹 ", color = "green" },
       },
+    })
+    local keys = {
       {
         "<leader>mb",
         function()
-          require("grapple").tag()
+          grapple_update_maps("tag")
         end,
         desc = "Bookmark file",
       },
       {
         "<leader>md",
         function()
-          require("grapple").untag()
+          grapple_update_maps("untag")
         end,
         desc = "Remove bookmark",
+        icon = { icon = "󰅖 ", color = "red" },
       },
       {
         "<leader>ml",
@@ -31,23 +80,18 @@ return {
       {
         "<leader>mc",
         function()
-          require("grapple").reset()
+          grapple_update_maps("reset")
         end,
         desc = "Clear bookmarks",
+        icon = { icon = " ", color = "red" },
       },
     }
-    for i = 1, 5 do
-      table.insert(keys, {
-        "<leader>m" .. i,
-        function()
-          if require("grapple").exists({ index = i }) then
-            require("grapple").select({ index = i })
-          end
-        end,
-        desc = "Goto bookmark " .. i,
-      })
-    end
-    return keys
+    Utils.map.set_keymaps(keys, { icon = "󰓹 ", color = "green" })
+    set_index_keys()
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      callback = function()
+        set_index_keys()
+      end,
+    })
   end,
-  opts = {},
 }
