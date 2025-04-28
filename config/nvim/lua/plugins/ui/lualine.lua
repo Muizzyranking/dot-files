2ac2da9a6f3b6e93d932c8b76fd0ff837ff2e451
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-04-28 03:44:51 +0100

    grapple

--- config/nvim/lua/plugins/ui/lualine.lua
+++ config/nvim/lua/plugins/ui/lualine.lua
@@ -68,7 +68,11 @@ return {
             },
           },
         },
-        lualine_c = {},
+        lualine_c = {
+          {
+            "grapple",
+          },
+        },
         lualine_x = {
           {
             function()
