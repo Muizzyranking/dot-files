9278bff624a2fcdc2fee162fe86103c7ccbdbe33
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-05-08 14:16:41 +0100

    noice

--- config/nvim/lua/plugins/ui/noice.lua
+++ config/nvim/lua/plugins/ui/noice.lua
@@ -13,7 +13,13 @@ return {
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true,
       },
-      signature = { enabled = true },
+      signature = {
+        enabled = true,
+        auto_open = {
+          enabled = false,
+          trigger = true,
+        },
+      },
     },
     routes = {
       {
