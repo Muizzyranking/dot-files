7c894a399405b129e6de0cb3621d6189f721aa46
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-06-30 22:04:04 +0100

    scripts

--- bin/restart_hypr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-systemctl --user stop xdg-desktop-portal-hyprland
-systemctl --user stop xdg-desktop-portal-gtk
-systemctl --user stop xdg-desktop-portal
-sleep 2
-systemctl --user start xdg-desktop-portal-hyprland
-systemctl --user start xdg-desktop-portal-gtk
-systemctl --user start xdg-desktop-portal
-
