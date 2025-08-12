ba5e78328bcd7dd812372bb51eeb8c1160dce561
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-12 18:14:07 +0100

    link

--- link.sh
+++ link.sh
@@ -122,7 +122,7 @@ link_zsh_files() {
 
 
 # Available config files
-configs=("bat" "git" "hypr" "kitty" "lazygit" "lazyvim" "nvim" "rofi" "swaync" "tmux" "waybar" "wlogout" "zsh")
+configs=("bat" "git" "hypr" "kitty" "lazygit" "lazyvim" "my_nvim" "nvim" "rofi" "swaync" "tmux" "waybar" "wlogout" "zsh")
 
 # If no arguments provided, show usage
 if [ $# -eq 0 ]; then
