7dc512f3b836f72edde1be2b85e1924995d05969
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-25 08:02:54 +0100

    WIP: minimal neovim from scratch

--- config/my_nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-local set = vim.keymap.set
-vim.keymap.set("n", "<leader>e", "<cmd>Lex 30<cr>")
-set({ "n", "i", "v" }, "<c-s>", "<cmd>w<cr><esc>", { silent = true, desc = "Save file" })
-set("n", "<c-q>", "<cmd>q<cr>", { silent = true, desc = "Quit" })
