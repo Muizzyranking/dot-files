e4947bf8e238a188cf54945a22c3a1ed77a154fc
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-21 00:00:49 +0100

    format

--- config/nvim/lua/plugins/lang/typescript.lua
+++ config/nvim/lua/plugins/lang/typescript.lua
@@ -184,22 +184,14 @@ return {
         vim.api.nvim_feedkeys("t", "n", true) -- pass through the trigger char
         local col = vim.api.nvim_win_get_cursor(0)[2]
         local text_before_cursor = vim.api.nvim_get_current_line():sub(col - 3, col)
-        if text_before_cursor ~= "awai" then
-          return
-        end
+        if text_before_cursor ~= "awai" then return end
 
-        local func_node = Utils.ts.find_node({ "arrow_function", "function_declaration", "function" })
-        if not func_node then
-          return
-        end
+        local func_node = Utils.treesitter.find_node({ "arrow_function", "function_declaration", "function" })
+        if not func_node then return end
         local func_text = vim.treesitter.get_node_text(func_node, 0)
-        if not func_text then
-          return
-        end
+        if not func_text then return end
 
-        if vim.startswith(func_text, "async") then
-          return
-        end -- already async
+        if vim.startswith(func_text, "async") then return end -- already async
 
         local start_row, start_col = func_node:start()
         vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { "async " })
