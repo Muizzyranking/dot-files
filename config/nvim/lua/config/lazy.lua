ab9aaf6152b9946d193c2cbfd13b77624af2d093
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-10-08 18:47:00 +0100

    feat: kualala http plugin

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "plugins.ai.copilot" },
     -- { import = "plugins.extras.neotest" },
     { import = "plugins.extras.refactoring" },
+    { import = "plugins.extras.kulala" },
     { import = "plugins" },
   },
   defaults = {
