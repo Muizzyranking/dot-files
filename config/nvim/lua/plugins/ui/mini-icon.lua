3ce6ab54332be6bf2af774578489a98a7674c2b3
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-10-08 18:44:26 +0100

    feat:mini icons

--- config/nvim/lua/plugins/ui/mini-icon.lua
+++ config/nvim/lua/plugins/ui/mini-icon.lua
@@ -9,9 +9,21 @@ return {
   opts = {
     file = {
       [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+      [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+      [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+      [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+      [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+      ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+      ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+      ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+
+      [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
     },
     filetype = {
       dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
       -- FIX: doesnt work
       htmldjango = { glyph = "", hl = "MiniIconsBlue" },
     },
