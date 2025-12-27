306d57383a0eb5b49420e2097409c51d47c993e3
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-12-27 18:52:51 +0100

    feat: colorscheme

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,98 @@
+return {
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    opts = {
+      dark_variant = "moon",
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
+      },
+      highlight_groups = {
+        Keyword = { fg = "#f7768e", italic = true },
+        String = { fg = "#9ece6a", italic = true },
+        ["@string.documentation"] = { fg = "#ff9e64" },
+        Operator = { fg = "#7aa2f7" },
+        ["@keyword.return"] = { fg = "#f7768e" },
+        ["@keyword.conditional"] = { fg = "#db4b4b" },
+        ["@keyword.import"] = { fg = "#f7768e" },
+        ["@type.builtin"] = { fg = "#db4b4b" },
+        ["@type"] = { fg = "#db4b4b" },
+        Type = { fg = "#db4b4b" },
+      },
+    },
+    config = function(_, opts)
+      require("rose-pine").setup(opts)
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+    opts = {
+      flavour = "mocha",
+      styles = {
+        comments = { "italic" },
+        functions = { "italic" },
+        keywords = { "italic" },
+        strings = { "italic" },
+        variables = { "italic" },
+      },
+      transparent_background = true,
+      default_integrations = true,
+      integrations = {
+        blink_cmp = true,
+        dropbar = { enabled = true },
+        dashboard = true,
+        harpoon = true,
+        -- fidget = true,
+        cmp = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        leap = true,
+        overseer = true,
+        markdown = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        treesitter = true,
+        which_key = true,
+      },
+      highlight_overrides = {
+        mocha = function()
+          return {
+            ["@string.documentation"] = { fg = "#ff9e64" },
+            Keyword = { fg = "#f7768e", italic = true },
+            String = { fg = "#9ece6a", italic = true },
+            Operator = { fg = "#7aa2f7" },
+            ["@keyword.return"] = { fg = "#f7768e" },
+            ["@keyword.conditional"] = { fg = "#db4b4b" },
+            ["@type.builtin"] = { fg = "#db4b4b" },
+            ["@type"] = { fg = "#db4b4b" },
+            Type = { fg = "#db4b4b" },
+          }
+        end,
+      },
+    },
+  },
+}
+
