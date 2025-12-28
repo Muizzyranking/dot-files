15fd37c5b35485a096b4229e09438c6f8bc7a913
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-12-28 12:14:56 +0100

    feat: abbrevations

---
+++ config/nvim/lua/config/abbrevations.lua
@@ -0,0 +1,8 @@
+Utils.map.abbrev({
+  { "don't", "dont" },
+  { "Don't", "Dont" },
+  { "local", { "lcaol", "lcoal", "locla" } },
+  { "share", { "saher", "sahre" } },
+  { "blame", { "balme" } },
+  { "return", { "Return" } },
+}, {})
