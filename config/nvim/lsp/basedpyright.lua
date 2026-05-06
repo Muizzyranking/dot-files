294cbeff49ce02ea94c8d0aa331945e2d6d95cc6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-05-06 19:24:37 +0100

    refactor: remove on attach basedpyright

--- config/nvim/lsp/basedpyright.lua
+++ config/nvim/lsp/basedpyright.lua
@@ -26,26 +26,4 @@ return {
       disableOrganizeImports = true,
     },
   },
-  on_attach = function(client, bufnr)
-    local root = Utils.root(bufnr)
-    local venv = Utils.lang.py.detect_and_activate_venv(root)
-    if venv and venv.python_path then
-      local new_settings = vim.tbl_deep_extend("force", client.config.settings or {}, {
-        python = { pythonPath = venv.python_path },
-      })
-
-      client.config.settings = new_settings
-      client:notify("workspace/didChangeConfiguration", { settings = new_settings })
-    end
-    local augroup = vim.api.nvim_create_augroup("basedpyright_config_" .. bufnr, { clear = true })
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      group = augroup,
-      pattern = "pyrightconfig.json",
-      callback = function()
-        Utils.lsp.restart("basedpyright")
-        vim.cmd("stopinsert")
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
-      end,
-    })
-  end,
 }
